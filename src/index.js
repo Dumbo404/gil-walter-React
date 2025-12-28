ade6976a28b522494aca7b000d59a4c510e6bf1d
GilWalter <oliver.gwbasse@gmail.com>
2025-12-28 11:32:50 +0100

    Commit from TeleportHQ

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+
+import './style.css'
+import Home from './views/home'
+import Kontakt from './views/kontakt'
+import Gedichte from './views/gedichte'
+import NotFound from './views/not-found'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route component={Home} exact path="/" />
+        <Route component={Kontakt} exact path="/kontakt" />
+        <Route component={Gedichte} exact path="/gedichte" />
+        <Route component={NotFound} path="**" />
+        <Redirect to="**" />
+      </Switch>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ src/views/kontakt.js
@@ -0,0 +1,239 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Helmet } from 'react-helmet'
+
+import './kontakt.css'
+
+const Kontakt = (props) => {
+  return (
+    <div className="kontakt-container10">
+      <Helmet>
+        <title>Kontakt - gil walter</title>
+        <meta property="og:title" content="Kontakt - gil walter" />
+        <meta property="og:description" content="Texte und Gedichte :) " />
+        <link
+          rel="canonical"
+          href="https://trim-tepid-oyster-w9bx29.teleporthq.app/kontakt"
+        />
+      </Helmet>
+      <div className="kontakt-thq-header-elm">
+        <header className="kontakt-container11">
+          <Link to="/" className="kontakt-navlink">
+            <h1 className="kontakt-text10">Gil Walter </h1>
+          </Link>
+        </header>
+      </div>
+      <div className="kontakt-container12">
+        <div className="kontakt-container13">
+          <span className="kontakt-text11">Projekte (Auswahl) </span>
+          <div data-thq="accordion" className="kontakt-accordion1">
+            <details
+              data-thq="accordion-trigger"
+              className="kontakt-thq-trigger-elm1"
+            >
+              <summary
+                data-thq="accordion-summary"
+                className="kontakt-thq-summary-elm1"
+              >
+                <span>ALFONS </span>
+                <div
+                  data-thq="accordion-icon"
+                  className="kontakt-thq-icon-container-elm1"
+                >
+                  <svg width="32" height="32" viewBox="0 0 24 24">
+                    <path d="m12 14l-4-4h8z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </summary>
+            </details>
+            <div data-thq="accordion-content">
+              <div className="kontakt-container14">
+                <p className="kontakt-text13">Roman, Drehbuch Langspielfilm </p>
+                <p className="kontakt-text14">Autor </p>
+                <p className="kontakt-text15">fertige Skripte </p>
+              </div>
+            </div>
+          </div>
+          <div data-thq="accordion" className="kontakt-accordion2">
+            <details
+              data-thq="accordion-trigger"
+              className="kontakt-thq-trigger-elm2"
+            >
+              <summary
+                data-thq="accordion-summary"
+                className="kontakt-thq-summary-elm2"
+              >
+                <span>Herr S. Trick </span>
+                <div
+                  data-thq="accordion-icon"
+                  className="kontakt-thq-icon-container-elm2"
+                >
+                  <svg width="32" height="32" viewBox="0 0 24 24">
+                    <path d="m12 14l-4-4h8z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </summary>
+            </details>
+            <div data-thq="accordion-content">
+              <div className="kontakt-container15">
+                <p className="kontakt-text17">Kurzgeschichte </p>
+              </div>
+              <div className="kontakt-container16">
+                <p className="kontakt-text18">fertiges Skript </p>
+              </div>
+            </div>
+          </div>
+          <div data-thq="accordion" className="kontakt-accordion3">
+            <details
+              data-thq="accordion-trigger"
+              className="kontakt-thq-trigger-elm3"
+            >
+              <summary
+                data-thq="accordion-summary"
+                className="kontakt-thq-summary-elm3"
+              >
+                <span className="kontakt-text19">Otto der Trotz-Otter </span>
+                <div
+                  data-thq="accordion-icon"
+                  className="kontakt-thq-icon-container-elm3"
+                >
+                  <svg
+                    width="32"
+                    height="32"
+                    viewBox="0 0 24 24"
+                    className="kontakt-icon14"
+                  >
+                    <path d="m12 14l-4-4h8z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </summary>
+            </details>
+            <div data-thq="accordion-content">
+              <div className="kontakt-container17">
+                <p className="kontakt-text20">Bilderbuch </p>
+              </div>
+              <div className="kontakt-container18">
+                <p className="kontakt-text21">fertiges Skript </p>
+              </div>
+            </div>
+          </div>
+          <div data-thq="accordion" className="kontakt-accordion4">
+            <details
+              data-thq="accordion-trigger"
+              className="kontakt-thq-trigger-elm4"
+            >
+              <summary
+                data-thq="accordion-summary"
+                className="kontakt-thq-summary-elm4"
+              >
+                <span>Nightracer </span>
+                <div
+                  data-thq="accordion-icon"
+                  className="kontakt-thq-icon-container-elm4"
+                >
+                  <svg width="32" height="32" viewBox="0 0 24 24">
+                    <path d="m12 14l-4-4h8z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </summary>
+            </details>
+            <div data-thq="accordion-content">
+              <div className="kontakt-container19">
+                <p>Kurzfilm </p>
+              </div>
+              <div className="kontakt-container20">
+                <p>Autor, Co-Producer </p>
+              </div>
+              <div className="kontakt-container21">
+                <p>2021 </p>
+              </div>
+            </div>
+          </div>
+          <div data-thq="accordion" className="kontakt-accordion5">
+            <details
+              data-thq="accordion-trigger"
+              className="kontakt-thq-trigger-elm5"
+            >
+              <summary
+                data-thq="accordion-summary"
+                className="kontakt-thq-summary-elm5"
+              >
+                <span>Lutalica </span>
+                <div
+                  data-thq="accordion-icon"
+                  className="kontakt-thq-icon-container-elm5"
+                >
+                  <svg width="32" height="32" viewBox="0 0 24 24">
+                    <path d="m12 14l-4-4h8z" fill="currentColor"></path>
+                  </svg>
+                </div>
+              </summary>
+            </details>
+            <div data-thq="accordion-content">
+              <div className="kontakt-container22">
+                <p className="kontakt-text27">Kurzfilm </p>
+                <p className="kontakt-text28">Autor </p>
+                <p className="kontakt-text29">2019</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="kontakt-container23">
+        <span className="kontakt-text30">Ausbildung </span>
+        <span className="kontakt-text31">
+          Master of Law (MLaw), Universität Bern, Februar 2024- Mai 2025
+        </span>
+        <span>Im Schwerpunkt Strafrecht und Kriminologie</span>
+        <span>Mit dem Prädikat magna cum laude</span>
+        <span className="kontakt-text34">
+          Und der Auszeichnung für die beste Masterarbeit im Departement der
+          Grundlagenfächer 
+        </span>
+        <span className="kontakt-text35">
+          Bachelor of Law (BLaw), Universität Bern, September 2019 – Juni 2023
+        </span>
+        <span className="kontakt-text36">Mit dem Prädikat magna cum laude</span>
+      </div>
+      <div className="kontakt-thq-footer-elm">
+        <a
+          href="mailto:oliver.gwbasse@gmail.com?subject="
+          className="kontakt-link1"
+        >
+          oliver.gwbasse@gmail.com
+        </a>
+        <span className="kontakt-text37">Zürich</span>
+        <span className="kontakt-text38">Datenschutzerklärung</span>
+      </div>
+      <a href="https://play.teleporthq.io/signup" className="kontakt-link2">
+        <div aria-label="Sign up to TeleportHQ" className="kontakt-container24">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 19 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="kontakt-icon20"
+          >
+            <path
+              d="M9.1017 4.64355H2.17867C0.711684 4.64355 -0.477539 5.79975 -0.477539 7.22599V13.9567C-0.477539 15.3829 0.711684 16.5391 2.17867 16.5391H9.1017C10.5687 16.5391 11.7579 15.3829 11.7579 13.9567V7.22599C11.7579 5.79975 10.5687 4.64355 9.1017 4.64355Z"
+              fill="#B23ADE"
+            ></path>
+            <path
+              d="M10.9733 12.7878C14.4208 12.7878 17.2156 10.0706 17.2156 6.71886C17.2156 3.3671 14.4208 0.649963 10.9733 0.649963C7.52573 0.649963 4.73096 3.3671 4.73096 6.71886C4.73096 10.0706 7.52573 12.7878 10.9733 12.7878Z"
+              fill="#FF5C5C"
+            ></path>
+            <path
+              d="M17.7373 13.3654C19.1497 14.1588 19.1497 15.4634 17.7373 16.2493L10.0865 20.5387C8.67402 21.332 7.51855 20.6836 7.51855 19.0968V10.5141C7.51855 8.92916 8.67402 8.2807 10.0865 9.07221L17.7373 13.3654Z"
+              fill="#2874DE"
+            ></path>
+          </svg>
+          <span className="kontakt-text39">Built in TeleportHQ</span>
+        </div>
+      </a>
+    </div>
+  )
+}
+
+export default Kontakt
